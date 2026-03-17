cbaf57472fbfdbdc191b60d066c359d0b4cec96d
iuli <iulia.stoichescu25@gmail.com>
2026-03-17 10:17:46 +0200

    Port change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 app.set('query parser', 'extended');
 const path = require('path');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const ExpressError = require('./utils/ExpressError');
